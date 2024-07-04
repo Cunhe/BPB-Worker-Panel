0736e3b3c0f49fdadd13251a514b4d7a8a1dfb71
Cunhe <161113619+Cunhe@users.noreply.github.com>
2024-07-04 17:45:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '17a62d76-34fd-4f37-86cd-c1e68368d62b';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
